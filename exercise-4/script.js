5b289e735a59b609d95250948a85925f3ec4480d
brynnaj <brynnaj78@gmail.com>
2023-11-09 13:56:50 -0500

    exercise 4

---
+++ exercise-4/script.js
@@ -0,0 +1,9 @@
+function createMultiplier (multiplier) {
+    return function (number) {
+        return number * multiplier;
+    };
+
+}
+
+const multiplyByFive = createMultiplier(10)
+console.log(multiplyByFive(5))
